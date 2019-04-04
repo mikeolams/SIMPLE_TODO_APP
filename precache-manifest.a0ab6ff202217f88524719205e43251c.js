070ee746f6f371802f72acd25d7467f99bcd8f7e
GMike the GD <mikeolams@yahoo.com>
2019-04-04 20:30:38 +0100

    Updates

---
+++ precache-manifest.a0ab6ff202217f88524719205e43251c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "83e5c8fe6489db9f9286",
+    "url": "/static/js/main.a5c2cc12.chunk.js"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "83e5c8fe6489db9f9286",
+    "url": "/static/css/main.488f8b2b.chunk.css"
+  },
+  {
+    "revision": "68f6c6d601798a3f00821cfb7b95f6dd",
+    "url": "/index.html"
+  }
+];
